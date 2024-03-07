834acf8509f6974315152aa5cd099e79dfafe9d2
Celso Mihara <ckmihara@gmail.com>
2024-03-06 21:29:20 -0300

    User entity and resource

---
+++ src/main/java/com/miharaweb/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.miharaweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.miharaweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> finAll() {
+		User u = new User(1L,"Maria","maria@gmail.com", "9999-9999", "0000000");
+		return ResponseEntity.ok().body(u);
+	}
+}
